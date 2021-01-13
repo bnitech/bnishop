195a7c5f83a3560f41254322ba71573f807b23e1
bnitech <bnitech88@gmail.com>
2021-01-14 07:36:41 +0900

    회원 도메인 개발 및 테스트

---
+++ src/test/java/bnilive/bnishop/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package bnilive.bnishop.service;
+
+import bnilive.bnishop.domain.Member;
+import bnilive.bnishop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() throws Exception {
+        // region given
+        Member member = new Member();
+        member.setName("kim");
+        // endregion
+
+        // region when
+        Long saveId = memberService.join(member);
+        // endregion
+
+        // region then
+        assertEquals(member, memberRepository.findOne(saveId));
+        // endregion
+    }
+
+    @Test
+    void 중복_회원_예외() throws Exception {
+        // region given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        Exception expectedException = new IllegalStateException("이미 존재하는 회원입니다.");
+        // endregion
+
+        // region when
+        memberService.join(member1);
+        Exception actualException = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        // endregion
+
+        // region then
+        assertEquals(expectedException.getMessage(), actualException.getMessage());
+        // endregion
+    }
+}
