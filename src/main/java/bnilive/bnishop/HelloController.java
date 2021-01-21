d7e46cd34ab92526dcf2e1854292148dd1747da3
bnitech <bnitech88@gmail.com>
2021-01-21 10:20:26 +0900

    회원 등록 & 회원 목록 조회

--- src/main/java/bnilive/bnishop/HelloController.java
+++ src/main/java/bnilive/bnishop/HelloController.java
@@ -1,17 +1,16 @@
 package bnilive.bnishop;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Slf4j
 public class HelloController {
 
-  @RequestMapping("/")
-  public String home() {
-    log.info("home controller");
-    return "home";
+  @GetMapping("hello")
+  public String hello(Model model) {
+    model.addAttribute("data", "hello!!");
+    return "hello";
   }
 }
 
