6c7368b374c68f851ea275e994ce410a2ce9b09f
bnitech <bnitech88@gmail.com>
2021-02-05 07:13:53 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/bnilive/bnishop/controller/OderSimpleApiController.java
+++ src/main/java/bnilive/bnishop/controller/OderSimpleApiController.java
@@ -1,9 +1,15 @@
 package bnilive.bnishop.controller;
 
+import static java.util.stream.Collectors.toList;
+
+import bnilive.bnishop.domain.Address;
 import bnilive.bnishop.domain.Order;
 import bnilive.bnishop.domain.OrderSearch;
+import bnilive.bnishop.domain.OrderStatus;
 import bnilive.bnishop.repository.OrderRepository;
+import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +36,29 @@ public class OderSimpleApiController {
     return all;
   }
 
+  @GetMapping("/api/v2/simple-orders")
+  public List<SimpleOrderDto> ordersV2() {
+    // ORDER 2개
+    // N + 1 -> 1 + 회원 N + 배송 N
+    List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+    return orders.stream().map(SimpleOrderDto::new).collect(toList());
+  }
+
+  @Data
+  static class SimpleOrderDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate; //주문시간
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public SimpleOrderDto(Order order) {
+      orderId = order.getId();
+      name = order.getMember().getName();
+      orderDate = order.getOrderDate();
+      orderStatus = order.getStatus();
+      address = order.getDelivery().getAddress();
+    }
+  }
 }
--- src/main/java/bnilive/bnishop/repository/OrderRepository.java
+++ src/main/java/bnilive/bnishop/repository/OrderRepository.java
@@ -14,6 +14,7 @@ import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
