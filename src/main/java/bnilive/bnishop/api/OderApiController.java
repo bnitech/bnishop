ff8e73b3ad1e83079faa1a8c405c211aa98e15bb
bnitech <bnitech88@gmail.com>
2021-03-03 07:17:46 +0900

    주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/bnilive/bnishop/api/OderApiController.java
+++ src/main/java/bnilive/bnishop/api/OderApiController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -49,6 +50,16 @@ public class OderApiController {
     return orderRepository.findAllWithItem().stream()
         .map(OrderDto::new)
         .collect(toList());
+  }
+
+  @GetMapping("/api/v3.1/orders")
+  public List<OrderDto> ordersV3_page(
+      @RequestParam(value = "offset", defaultValue = "0") int offset,
+      @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+    List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+    return orders.stream().map(OrderDto::new).collect(toList());
   }
 
   @Data
